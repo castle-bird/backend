eeedb3f2203e706efff2223eb074ab315be1722b
castle-bird <seongjo0501@gmail.com>
2026-04-12 16:43:46 +0900

    feat: Spring Security 설정 추가 및 의존성 업데이트
    - Spring Security 초기 설정 추가 (`SecurityConfig` 클래스 생성)
    - JWT 사용을 위한 `io.jsonwebtoken` 의존성 추가
    - Stateless 세션 관리 및 기본 CORS 설정 구현

---
+++ src/main/java/io/project/backend/global/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package io.project.backend.global.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests(authorizeCustomizer())
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(sessionManagementCustomizer())
+        .cors(corsCustomizer())
+        .build();
+  }
+
+  // 권한 경로 설정
+  private Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizeCustomizer() {
+    return auth -> auth
+        .requestMatchers(
+            "/",
+            "/login",
+            "/join"
+        ).permitAll()
+        .anyRequest().authenticated();
+  }
+
+  // CORS
+  private Customizer<CorsConfigurer<HttpSecurity>> corsCustomizer() {
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowedOrigins(List.of(
+        "http://localhost:8080",   // 서버 확인용
+        "http://localhost:5173"    // React (Vite)
+    ));
+    config.setAllowedMethods(List.of("*"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return cors -> cors.configurationSource(source);
+  }
+
+  // Session
+  private Customizer<SessionManagementConfigurer<HttpSecurity>> sessionManagementCustomizer() {
+    return session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
