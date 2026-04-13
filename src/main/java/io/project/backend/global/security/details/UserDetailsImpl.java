8897d2aa24eac7668cf6c7bf57a9d29f72887e70
castle-bird <seongjo0501@gmail.com>
2026-04-13 18:24:04 +0900

    feat: Spring Security UserDetails 및 JWT 필터 구현
    - UserDetailsImpl 및 UserDetailsServiceImpl 클래스 추가로 사용자 인증 정보 관리
    - JwtAuthenticationFilter 클래스 추가로 JWT 기반 인증 필터 구현

---
+++ src/main/java/io/project/backend/global/security/details/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package io.project.backend.global.security.details;
+
+import io.project.backend.domain.employee.entity.Employee;
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+  private final Employee employee;
+
+  @Override
+  public String getUsername() {
+    return employee.getEmail();
+  }
+
+  @Override
+  public String getPassword() {
+    return employee.getPassword();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE" + employee.getRole()));
+  }
+
+  // 계정 만료 여부. e.g. 장기 미접속 등
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  // 계정 잠금 여부. e.g. 비밀번호 여러 번 틀림 등
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  // 자격 증명 만료 여부. e.g. 비밀번호 유효 기간 만료 등
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  // 계정 활성화 여부. e.g. 이메일 인증 완료 여부, 회원 탈퇴 여부 등
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
---
+++ src/main/java/io/project/backend/global/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package io.project.backend.global.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private static final String BEARER_PREFIX = "Bearer ";
+
+  private final JwtProvider jwtProvider;
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain
+  ) throws ServletException, IOException {
+
+    filterChain.doFilter(request, response);
+  }
+}
