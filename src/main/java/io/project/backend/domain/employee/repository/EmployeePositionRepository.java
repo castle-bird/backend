b6de49581961eea4a8c05018b47ad0d1329ca52e
castle-bird <seongjo0501@gmail.com>
2026-04-28 09:17:26 +0900

    refactor: @Repository 어노테이션 누락 작성

--- src/main/java/io/project/backend/domain/employee/repository/EmployeePositionRepository.java
+++ src/main/java/io/project/backend/domain/employee/repository/EmployeePositionRepository.java
@@ -4,7 +4,9 @@ import io.project.backend.domain.employee.entity.EmployeePosition;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeePositionRepository extends JpaRepository<EmployeePosition, Long> {
 
   Optional<EmployeePosition> findByName(String name);
